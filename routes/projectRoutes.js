d10b7880f2a4fe7c8f8d8ca60950434fc4febd71
kritikaVijaysinghRajput <kritikarajput203@gmail.com>
2024-10-30 15:48:34 +0530

    backend update

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,15 +1,10 @@
 import express from "express";
+import projects from "../data/data.json" assert { type: "json" };
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Welcome to the Projects API!" });
+  res.json(projects);
 });
 
-router.get("/:id", (req, res) => {
-  const projectId = req.params.id;
-  res.json({ message: `Fetching details for project with ID: ${projectId}` });
-});
-
-// Export the router
 export default router;
