929045acac925ab5651bc5d864e7675eaa5ff8aa
kritikaVijaysinghRajput <kritikarajput203@gmail.com>
2024-10-30 15:02:29 +0530

    backend update

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// server/index.js
 const express = require("express");
 const cors = require("cors");
 const projectRoutes = require("./routes/projectRoutes");
+const projectData = require("./data/data.json");
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json());
 
-// Use project routes
 app.use("/api/projects", projectRoutes);
+app.get("/api/projects", (req, res) => {
+  res.json(projectData);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
